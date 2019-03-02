24536f377b06dee9cdb5afd9a03f35bfa6444857
tchret <tchret@gmail.com>
2019-03-02 10:42:09 +0200

    fix infinite scroll

--- plugins/globale.js
+++ plugins/globale.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 
-export const apiUrl = process.env.NODE_ENV === 'development' ? 'http://lvh.me:3000/v1' : 'https://api.pages.xyz/v1'
+export const apiUrl = process.env.NODE_ENV === 'development' ? 'http://lvh.me:3000/v1' : 'https://pagesxyzapi.herokuapp.com/v1'
 
 Vue.mixin({
   data () {

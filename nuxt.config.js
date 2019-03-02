ae7c527f55a770efe1cdb991d42729e6d7766ec8
tchret <tchret@gmail.com>
2019-03-02 10:57:25 +0200

    new config

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,7 +33,7 @@ module.exports = {
   ],
 
   axios: {
-    baseURL: 'https://pagesxyzapi.herokuapp.com/v1'
+    baseURL: process.env.BASE_URL || 'https://pagesxyzapi.herokuapp.com/v1'
   },
 
   loading: false,
